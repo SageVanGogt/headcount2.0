71940b5d40cd826a41cbe0d53b611d790e292ed4
Sage Vogt <thurmanvogt@gmail.com>
2018-05-06 22:27:00 -0600

    Add basic app tests
    testing it renders correctly and snapshot

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import DistrictCard from './DistrictCard';
 import { shallow, mount } from 'enzyme';
-import '../setupTests';
+// import '../setupTests';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-});
+describe('App test', () => {
+  it('Should render all the components', () => {
+    const appComponent = shallow(<App />)
+ 
+    expect(appComponent.find('Search').length).toEqual(1)
+    expect(appComponent.find('CompareDistricts').length).toEqual(1)
+    expect(appComponent.find('DistrictsContainer').length).toEqual(1)
+  });
+
+  it('Should match the snapshot', () => {
+    const appComponent = shallow(<App />)
+
+    expect(appComponent).toMatchSnapshot()
+  })
+
+  it('should render with the correct default state', () => {
+    const appComponent = shallow(<App />)
+
+    expect(Object.keys(appComponent.state('districts')).length).toEqual(181);
+    expect(appComponent.state('selectedDistricts')).toEqual([]);
+    expect(appComponent.state('comparedDistricts')).toEqual({})
+  })
+})
